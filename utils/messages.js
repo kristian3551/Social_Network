d158c302fb59a942d2f838dfcc4f6109d4a30085
kristian3551 <svktodorovi@gmail.com>
2023-08-03 14:08:26 +0300

    added more auth messages

--- utils/messages.js
+++ utils/messages.js
@@ -2,9 +2,12 @@ const PAGE_NOT_PASSED = 'Page number has to be passed as query parameter!';
 const USER_CREATED = 'Successfully created user!';
 const PASSWORD_UPDATED = 'Successfully updated password!';
 const USER_NOT_FOUND = 'There is no such user!';
-const PASSWORD_INCORRECT = 'Passwords do not match';
-const USER_LOGGED_IN = 'Successfully logged in';
-const USER_LOGGED_OUT = 'Successfully logged out';
+const PASSWORD_INCORRECT = 'Passwords do not match!';
+const USER_LOGGED_IN = 'Successfully logged in!';
+const USER_LOGGED_OUT = 'Successfully logged out!';
+const USER_NOT_LOGGED = 'You are not logged in!';
+const USER_NOT_ADMIN = 'User is not admin!';
+const USER_NOT_END_USER = 'User is not end user!';
 
 module.exports = {
     PAGE_NOT_PASSED,
@@ -13,5 +16,8 @@ module.exports = {
     USER_NOT_FOUND,
     PASSWORD_INCORRECT,
     USER_LOGGED_IN,
-    USER_LOGGED_OUT
+    USER_LOGGED_OUT,
+    USER_NOT_LOGGED,
+    USER_NOT_ADMIN,
+    USER_NOT_END_USER
 };
