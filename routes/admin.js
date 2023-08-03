ae38a2d6dec3ce7146d9498ca4b1b6047814dd6c
kristian3551 <svktodorovi@gmail.com>
2023-08-03 20:39:49 +0300

    added validations to creating user

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,9 @@
 const controller = require('../controllers/admin');
 const router = require('express').Router();
-const { isAuth, isAdmin, isPasswordValid } = require('../middlewares');
+const { isAuth, isAdmin, isPasswordValid, isUsernameValid, isEmailValid } = require('../middlewares');
 
 router.get('/', isAuth, isAdmin, controller.get.users);
-router.post('/', isAuth, isAdmin, controller.post.user);
+router.post('/', isUsernameValid, isPasswordValid, isEmailValid, isAuth, isAdmin, controller.post.user);
 router.patch('/:id/password', isPasswordValid, isAuth, isAdmin, controller.patch.password);
 router.delete('/:id', isAuth, isAdmin, controller.delete.user);
 
