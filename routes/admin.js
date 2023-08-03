fc0870418f0e92b401047db4db2e11e36adea3a2
kristian3551 <svktodorovi@gmail.com>
2023-08-03 16:50:32 +0300

    added validation middlewares

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
 const controller = require('../controllers/admin');
 const router = require('express').Router();
-const { isAuth, isAdmin } = require('../middlewares');
+const { isAuth, isAdmin, isPasswordValid } = require('../middlewares');
 
 router.get('/', isAuth, isAdmin, controller.get.users);
 router.post('/', isAuth, isAdmin, controller.post.user);
-router.patch('/:id/password', isAuth, isAdmin, controller.patch.password);
+router.patch('/:id/password', isPasswordValid, isAuth, isAdmin, controller.patch.password);
 router.delete('/:id', isAuth, isAdmin, controller.delete.user);
 
 module.exports = router;
