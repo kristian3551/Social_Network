80bf0a5dd32c1ba83773971744a50f700dafc389
kristian3551 <svktodorovi@gmail.com>
2023-08-04 20:12:18 +0300

    added domain and staticDirname to config

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 module.exports = {
     port: process.env.PORT,
+    domain: `http://localhost:${process.env.PORT}`,
+    staticDirname: 'uploads',
     dbConfig: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DATABASE,
-        
         options: {
             host: process.env.DB_HOST,
             dialect: 'postgres'
